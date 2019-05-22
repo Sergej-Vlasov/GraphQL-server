1b3117fae47c62297dd9b8a2e04fad4ec87018e7
Sergej-Vlasov <sergej.s.vlasov@gmail.com>
2019-05-23 00:03:09 +0100

    create GraphQL schema and dummy data to test it

---
+++ schema.js
@@ -0,0 +1,57 @@
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLInt,
+    GraphQLSchema,
+    GraphQLList,
+    GraphQLNonNull
+} = require('graphql');
+
+//Hardcoded data
+const customers = [
+    {id:'1', name:'Eagle Doe', email:'edoe@gmail.com', age:35},
+    {id:'2', name:'Koala Smith', email:'koala@gmail.com', age:32},
+    {id:'3', name:'Panda Williams', email:'panda@gmail.com', age:19}
+];
+
+
+// Customer Type
+const CustomerType = new GraphQLObjectType({
+    name: 'Customer',
+    fields: () => ({
+        id: {type: GraphQLString},
+        name: {type: GraphQLString},
+        email: {type: GraphQLString},
+        age: {type: GraphQLInt}
+    })
+});
+
+// Root Query
+const RootQuery = new GraphQLObjectType({
+    name: 'RootQueryType',
+    fields: {
+        customer: {
+            type: CustomerType,
+            args: {
+                id: {type: GraphQLString}
+            },
+            resolve(parentValue, args){
+                for (let i = 0; i < customers.length; i++) {
+                    if (customers[i].id == args.id) {
+                        return customers[i];
+                    }
+                }
+            }
+        },
+        customers: {
+            type: new GraphQLList(CustomerType),
+            resolve(parentValue, args) {
+                return customers;
+            }
+        }
+    }
+});
+
+module.exports = new GraphQLSchema({
+    query: RootQuery
+});
